300f1552b084e535afa73d3b849b04ce1a0c1dde
Maxim Zhuk <zhukme84@gmail.com>
2024-01-16 22:41:22 +0500

    add task3

---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+string StopQ(string n)
+{   
+    string q = "q";
+    if ( n == q)
+    {
+       return "1"; 
+    }
+    return "2"; 
+} 
+int GetEvenNumber(int n)
+{
+    if ( n % 2 == 0)
+    {
+       return 1; 
+    }
+    return 2; 
+}
+
+while (true)
+{
+Console.Write("Введите целое цисло: ");
+string n = Console.ReadLine();
+StopQ(n);
+
+if ( StopQ(n) == "1")
+{
+    Console.WriteLine("Программа остановлена");
+    return; 
+}
+
+if (int.TryParse(n, out int number))
+{
+    if ( GetEvenNumber(Convert.ToInt32(n)) == 1)
+    {
+        Console.WriteLine("Число чётное. Программа остановлена.");
+        return; 
+    }
+}
+else
+{
+    Console.WriteLine("Недопустимое выражение"); 
+}
+}
+
+
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,13 +10,14 @@ void FillArray(int[] arr)
     }
 }
 
-// void PrintArray(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         Console.Write($"{arr[i]} ");
-//     }
-// }
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
 
 int GetNumberEvenElementsArray(int[] arr)
 {
@@ -35,5 +36,5 @@ int GetNumberEvenElementsArray(int[] arr)
 int n = 10;
 int[] arr = new int[n];
 FillArray(arr);
-// PrintArray(arr);
-Console.WriteLine($"{GetNumberEvenElementsArray(arr)} ");
+PrintArray(arr);
+Console.WriteLine($"Четных чисел в массиве: {GetNumberEvenElementsArray(arr)} ");
